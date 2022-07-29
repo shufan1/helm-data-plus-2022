32d0a4a204cb5a90d1017135e59e6ae01fd37050
elankford <99899642+elankford@users.noreply.github.com>
2022-07-29 11:02:54 -0400

    Delete trainUnet.sh

--- model/trainUnet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH -o slurm.out
-#SBATCH -e slurm.err
-#SBATCH --mem=30G # 4 GBs RAM 
-#SBATCH –p gpu-common
-python trainUnet.py -train ardmayle kilbixy -test knockainey -prop 0.8 -s 512 -lr 0.01 -m 0.9 -e 10 -b 10 -f -o knockainey
-
